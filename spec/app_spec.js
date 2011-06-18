78b3d2c23f4c99b5efba4de3ef67566aeb68882d
1db2d <1db2d@3de8bab7-eab6-4343-b732-d82f5b2926e0>
2011-06-18 13:11:26 +0000

    added first spec
    git-svn-id: svn+ssh://10.1.1.10/srv/repos/devm/aodev/branches/gr-18@233 3de8bab7-eab6-4343-b732-d82f5b2926e0

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,6 +1,30 @@
-describe('App', function() {
-  it('works!', function() {
-    expect(2 + 2).toEqual(4);
+describe('mystatic', function() {
+  var mystatic = require('../mystatic');
+
+  function fakeRequest(method, url) {
+    return {
+      url: url,
+      method: method
+    };
+  }
+
+  function fakeResponse() {
+    return {};
+  }
+
+  var server;
+
+  beforeEach(function() {
+    server = new mystatic.Server();
+  });
+
+  it('triggers a static event given a static file url', function() {
+    var callback = jasmine.createSpy();
+    server.on('static', callback);
+
+    server.handle(fakeRequest('GET', '/image.png') , fakeResponse());
+
+    expect(callback).toHaveBeenCalled();
   });
 
   it('returns filename', function() {
