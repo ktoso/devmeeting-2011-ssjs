22dcc9923a1a33f26ef4e209444710fc4954524c
1db2d <1db2d@3de8bab7-eab6-4343-b732-d82f5b2926e0>
2011-06-18 12:22:40 +0000

    No longer polluting the global namespace
    git-svn-id: svn+ssh://10.1.1.10/srv/repos/devm/aodev/branches/gr-18@202 3de8bab7-eab6-4343-b732-d82f5b2926e0

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-var http = require('http');
+(function(require, console) {
+  var http = require('http');
 
-var nodeStatic = require('node-static');
-var publicFiles = new nodeStatic.Server('./public');
+  var nodeStatic = require('node-static');
+  var publicFiles = new nodeStatic.Server('./public');
 
-function handleError(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end(request.url);
-}
+  function handleError(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(request.url);
+  }
 
-function isRightFileFormat(url) {
-  return url.match(/\.(png|css|jpg|html|gif|js)$/);
-}
+  function isRightFileFormat(url) {
+    return url.match(/\.(png|css|jpg|html|gif|js)$/);
+  }
 
-http.createServer(function (request, response) {
-  if(request.method !== 'GET') { return handleError(request, response); }
-  if(!isRightFileFormat(request.url)) { return handleError(request, response); }
+  http.createServer(function (request, response) {
+    if(request.method !== 'GET') { return handleError(request, response); }
+    if(!isRightFileFormat(request.url)) { return handleError(request, response); }
 
-  request.addListener('end', function () {
-    publicFiles.serve(request, response);
-  });
-}).listen(1337, "127.0.0.1");
+    request.addListener('end', function () {
+      publicFiles.serve(request, response);
+    });
+  }).listen(1337, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:1337/');
+  console.log('Server running at http://127.0.0.1:1337/');
+})(require, console);
