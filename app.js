6cbc3c55fdd0f79397d90a3ad0a7bba84e802699
Adam Pohorecki <adam.pohorecki@llp.pl>
2011-06-18 17:03:29 +0200

    Added handlers for static files and errors.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 (function(require, console) {
     var mystatic = require('./mystatic');
+    var nodeStatic = require('node-static');
+    var publicFiles = new nodeStatic.Server('./public');
 
     statSrv = new mystatic.Server({ addr: "127.0.0.1",
                                     port: 1337,
@@ -9,24 +11,27 @@
 
     statSrv.start();
 
-    function handleError(request, response) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end(request.url);
+    function log(msg) {
+      console.log(new Date().toTimeString() + ' ' + msg);
     }
 
-    statSrv.on('static', function(request, response) {
-        console.log('static content'+ request.url);
-        response.end();
-    });
+    function handleError(request, response) {
+      log("ERROR " + request.url);
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end(request.url);
+    }
+
+    function handleStatic(request, response) {
+      log("GET " + request.url);
+      publicFiles.serve(request, response);
+    }
 
     statSrv.on('dynamic', function(request, response) {
         console.log('dynamic content' + request.url);
         response.end();
     });
 
-    statSrv.on('error', function(request, response) {
-        console.log('error'+ request.url);
-        response.end();
-    });
+    statSrv.on('static', handleStatic);
+    statSrv.on('error', handleError);
 
 })(require, console);
