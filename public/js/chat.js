9afa8c54eafc47c5af9e7dff47be9130090109d9
Konrad Malawski <konrad.malawski@project13.pl>
2011-06-18 17:34:33 +0200

    some work with client side

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,16 @@ $(function() {
   socket.connect();
 
   socket.on('connect', function(){
+	
   });
-  socket.on('message', function(){
+
+  socket.on('message', function(data){
+//	case(data.type) {
+		alert(data)
+//	}	
   });
+
   socket.on('disconnect', function(){
+	
   });
 });
